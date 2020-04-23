f601a1e410a44102e75bec6f3e04c342bd93531a
0xsnipe <benjaminvanr@gmail.com>
2020-04-23 14:31:01 +0200

    mongo command and readme

--- Mongo.py
+++ Mongo.py
@@ -1,5 +1,5 @@
 import pymongo
-from helpers import colorPrint, logPretty
+from helpers import colorPrint, logPretty, getTimestampSeconds
 
 def initializeMongo():
     global mongoClient
@@ -66,6 +66,18 @@ def getTransactionsFromDatabase(filter_value, filter_type='blockHeight'):
 
     return transaction_list
 
+def getLastSavedTransactionsFromDatabase(limit):
+    global mongoClient
+    global mongoDatabase
+    global mongoCollectionTransactions
+    transaction_list = []
+
+    res = mongoCollectionTransactions.find().sort([('height', -1)]).limit(limit)
+    for transaction in res:
+        transaction_list.append(transaction)
+
+    return transaction_list
+
 def addEventToDatabase(event_dict):
     global mongoClient
     global mongoDatabase
@@ -73,6 +85,7 @@ def addEventToDatabase(event_dict):
     try:
         logPretty('Adding event to database run_id: {}'.format(event_dict['run_id']))
         return mongoCollectionEvents.insert_one({
+            'timestamp': getTimestampSeconds(),
             'run_id': event_dict['run_id'],
             'compliant_nodes_ids': event_dict['compliant_nodes_ids'],
             'defiant_nodes_ids': event_dict['defiant_nodes_ids'],
